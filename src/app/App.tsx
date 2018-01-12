6d70cab176287215abd6b0f8e4f6cc7ad52ae535
Berelindis <romain.bellande@orange.fr>
2018-01-13 00:52:01 +0100

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.scss';
+
+
+export class App extends React.Component<{}, {}> {
+  public render() {
+    return (
+      <section className="App">
+        Hello World !
+      </section>
+    );
+  }
+}
